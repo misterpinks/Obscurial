d12b2fe26dde9115de43cbc7695244aa64aa4224
misterpinks <misterpink@gmail.com>
2025-05-05 16:01:03 +1000

    Create forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: './assets/icon' // no file extension required
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        // The ICO file to use as the icon for the generated Setup.exe
+        setupIcon: './assets/icon.ico',
+        // An array of strings which are .exe files to sign
+        certificateFile: undefined,
+        certificatePassword: undefined
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['win32'],
+    }
+  ],
+};
